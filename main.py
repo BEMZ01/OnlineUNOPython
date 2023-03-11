d386951e92e2ea72157bec44b67f4807f41659a7
BEMZlabs <hello@bemz.info>
2023-03-11 20:04:03 +0000

    Uhh... Added stuff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
 import random
-
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from pprint import pprint
 
 #########################################
 # A standard UNO deck contains:         #
@@ -16,6 +18,8 @@ import random
 # 4 Blank cards                         #
 #########################################
 
+app = Flask(__name__)
+
 
 class Card():
     def __init__(self, type, color):
@@ -23,10 +27,10 @@ class Card():
         self.color = color
 
     def __str__(self):
-        return self.type[0].capitalize() + " " + self.color[0].capitalize()
+        return self.type + " " + self.color
 
     def __repr__(self):
-        return self.type + " " + self.color
+        return self.type[0].capitalize() + " " + self.color[0].capitalize()
 
     def __eq__(self, other):
         return self.type == other.type and self.color == other.color
@@ -95,12 +99,15 @@ class Game:
         elif card.color == top_card.color:
             return True
         elif card.type == top_card.type:
-            for i in range(0, self.settings["MaxStack"]):
-                # Check if a plus card is stacked on top of another plus card
-                if top_card.type == "Draw-Two" or top_card.type == "Skip" or top_card.type == "Reverse":
-                    top_card = self.DiscardDeck.GetCardAt(i + 1)
-                else:
-                    break
+            try:
+                for i in range(0, self.settings["MaxStack"]):
+                    # Check if a plus card is stacked on top of another plus card
+                    if top_card.type == "Draw-Two" or top_card.type == "Skip" or top_card.type == "Reverse":
+                        top_card = self.DiscardDeck.GetCardAt(i + 1)
+                    else:
+                        break
+            except IndexError:
+                pass
             if card.type == top_card.type:
                 return True
         elif card.type == "Blank":
@@ -112,7 +119,7 @@ class Game:
 class Player:
     def __init__(self, name):
         self.name = name
-        self.hand = []
+        self.hand = Deck()
 
     def __str__(self):
         return self.name
@@ -148,10 +155,19 @@ class Game_Host:
         self.game.MainDeck.shuffle()
         for player in self.game.players:
             for i in range(0, 7):
-                player.hand.append(self.game.MainDeck.cards.pop())
+                player.hand.insertAt(0, self.game.MainDeck.cards.pop())
         self.game.DiscardDeck.cards.append(self.game.MainDeck.cards.pop())
         while True:
             for player in self.game.players:
+                if self.game.DiscardDeck.GetTopCard().type == "Wild" or self.game.DiscardDeck.GetTopCard().type == "Wild-Draw-Four":
+                    print("The top card is a Wild card! What color would you like to change it to?")
+                    while True:
+                        color = input()
+                        if color.lower() == "red" or color.lower() == "blue" or color.lower() == "green" or color.lower() == "yellow":
+                            break
+                        else:
+                            print("Please enter a valid color!")
+                    self.game.DiscardDeck.GetTopCard().color = color.title()
                 if self.game.DiscardDeck.GetTopCard().type == "Reverse":
                     print("The direction of play has been reversed!")
                 print(player.name + "'s turn!")
@@ -159,44 +175,56 @@ class Game_Host:
                     # If there is a stack, tell the player how many cards they have to draw
                     print("You have to draw " + str(self.game.CurrentStackCount) + " cards!")
                     for i in range(0, self.game.CurrentStackCount):
-                        player.hand.append(self.game.MainDeck.cards.pop())
+                        player.hand.cards.append(self.game.MainDeck.cards.pop())
                     self.game.CurrentStackCount = 0
                 if self.game.SkipNextPlayer:
                     print("You have to skip your next turn!")
                     self.game.SkipNextPlayer = False
                     break
                 print("Your hand is: ")
-                for card in player.hand:
-                    print(str(player.hand.index(card)) + ": " + str(card))
+                for card in player.hand.cards:
+                    print(str(player.hand.cards.index(card)) + ": " + str(card))
                 print("The top card is: " + str(self.game.DiscardDeck.GetTopCard()))
                 print("What card do you want to play?")
-                while True:
+                while True:  # Input validation
                     try:
-                        card_number = int(input())
-                        if card_number > len(player.hand):
+                        card_number = input()
+                        try:
+                            card_number = int(card_number)
+                        except ValueError:
+                            if card_number == "draw":
+                                player.hand.cards.append(self.game.MainDeck.cards.pop())
+                                break
+                            else:
+                                print("Please enter a number!")
+                                continue
+                        if card_number > len(player.hand.cards):
                             print("That card doesn't exist!")
                         else:
-                            break
-                        card = player.hand.GetCardAt(card_number - 1)
-                        if not self.game.checkLegalMove(card):
-                            print("That card is not legal!")
+                            card = player.hand.GetCardAt(card_number)
+                            if not self.game.checkLegalMove(card):
+                                print("That card is not legal!")
+                            else:
+                                break
                     except ValueError:
                         print("Please enter a number!")
-                card = player.hand.pop(card_number - 1)
-                # Add the card to the top of the discard deck
-                self.game.DiscardDeck.cards.append(card)
-                if len(player.hand) == 0:
+                if type(card_number) == int:
+                    card = player.hand.cards.pop(card_number)
+                    # Add the card to the top of the discard deck
+                    self.game.DiscardDeck.insertAt(0, card)
+                if len(player.hand.cards) == 0:
                     print("Congratulations! You won!")
                     return
-                top_card = self.game.DiscardDeck.GetTopCard()
-                if top_card.type == "Draw-Two":
-                    self.game.CurrentStackCount += 2
-                elif top_card.type == "Wild-Draw-Four":
-                    self.game.CurrentStackCount += 4
-                elif top_card.type == "Skip":
-                    self.game.SkipNextPlayer = True
-                elif top_card.type == "Reverse":
-                    self.game.players.reverse()
+                if type(card_number) == int:  # If the player didn't draw a card
+                    top_card = self.game.DiscardDeck.GetTopCard()
+                    if top_card.type == "Draw-Two":
+                        self.game.CurrentStackCount += 2
+                    elif top_card.type == "Wild-Draw-Four":
+                        self.game.CurrentStackCount += 4
+                    elif top_card.type == "Skip":
+                        self.game.SkipNextPlayer = True
+                    elif top_card.type == "Reverse":
+                        self.game.players.reverse()
                 if len(self.game.MainDeck.cards) == 0:
                     self.game.MainDeck.cards = self.game.DiscardDeck.cards
                     self.game.DiscardDeck.cards = []
@@ -207,6 +235,141 @@ class Game_Host:
                     return
 
 
+#######################
+#######################
+#######################
+
+
+class Client:
+    def __init__(self, ip_address, name=None):
+        self.ip_address = ip_address
+        self.name = name
+
+
+class Room:
+    def __init__(self, name, host, password=None):
+        self.name = name
+        self.host = host
+        self.password = password
+        self.players = []
+        self.game = Game_Host()
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def remove_player(self, player):
+        self.players.remove(player)
+
+    def start_game(self):
+        self.game.start_game()
+
+    def modify_settings(self, settings):
+        self.game.settings = settings
+
+    def get_settings(self):
+        return self.game.settings
+
+
+class Room_Manager:
+    def __init__(self):
+        self.rooms = []
+
+    def get_room(self, name):
+        for room in self.rooms:
+            if room.name == name:
+                return room
+        return None
+
+    def delete_room(self, name):
+        for room in self.rooms:
+            if room.name == name:
+                self.rooms.remove(room)
+                return True
+        return False
+
+    def create_room(self, name, host, password=None):
+        if len(password) == 0:
+            password = None
+        self.rooms.append(Room(name, host, password))
+        return self.rooms[-1]
+
+
+def get_username(ip_address):
+    with open("storage/users.json", "r") as f:
+        users = json.load(f)
+    for user in users:
+        if user["ip_address"] == ip_address:
+            return user["name"]
+    return ip_address
+
+
+def set_username(ip_address, name):
+    with open("storage/users.json", "r") as f:
+        users = json.load(f)
+    for user in users:
+        # If IPaddress is already in the database overwrite the username
+        if user["ip_address"] == ip_address:
+            user["name"] = name
+            with open("storage/users.json", "w") as f:
+                json.dump(users, f)
+            return True
+    users.append({"ip_address": ip_address, "name": name})
+    with open("storage/users.json", "w") as f:
+        json.dump(users, f)
+    return True
+
+
+@app.route("/create_room", methods=["POST"])
+def create_room():
+    global rooms
+    data = request.form.to_dict()
+    pprint(data)
+    rooms.create_room(data["name"], request.remote_addr, data["password"])
+    return redirect(url_for("index"))
+
+
+@app.route("/")
+def index():
+    global rooms
+    return render_template("index.html", rooms=rooms.rooms, username=get_username(request.remote_addr))
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/room/<room_name>")
+def room(room_name):
+    global rooms
+    room = rooms.get_room(room_name)
+    return render_template("room.html", room=room)
+
+
+@app.route("/room/<room_name>/api/players", methods=["GET", "POST"])
+def api_players(room_name):
+    global rooms
+    room = rooms.get_room(room_name)
+    if request.method == "GET":
+        return jsonify(room.players)
+    else:
+        data = request.form.to_dict()
+        pprint(data)
+        room.add_player(Client(request.remote_addr, data["name"]))
+        return redirect(url_for("room", room_name=room_name))
+
+
+@app.route("/settings", methods=["GET", "POST"])
+def settings():
+    if request.method == "GET":
+        return render_template("settings.html", username=get_username(request.remote_addr), address=request.remote_addr)
+    else:
+        data = request.form.to_dict()
+        pprint(data)
+        set_username(request.remote_addr, data["username"])
+        return redirect(url_for("index"))
+
+
 if __name__ == "__main__":
-    game_host = Game_Host(AllowBlankCards=False)
-    game_host.start_game()
+    rooms = Room_Manager()
+    app.run("0.0.0.0", 5000, debug=True)
